462a90f27fd48a7b71ead1d317dbfb68e9a9f626
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2019-10-11 19:04:51 -0400

    Add admin creation.

--- django/app/views.py
+++ django/app/views.py
@@ -3,10 +3,31 @@ from django.views import View as RegView
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 from .images import list_images, inspect_image
+
+
+class AdminCreationView(APIView):
+    def get(self, request, format=None):
+        import random
+        import string
+
+        try:
+            User.objects.get(username="admin")
+            return Response("User exists")
+
+        except User.DoesNotExist:
+            a = User(username='admin', email="admin@localhost")
+            pw = ''.join(random.choices(
+                string.ascii_lowercase + string.digits + '!+-._', k=24))
+            a.set_password(pw)
+            a.is_superuser = True
+            a.is_staff = True
+            a.save()
+            return Response("admin:" + pw)
 
 
 class ListImagesView(APIView):
