e27e195d1f3939a84d28b8ec2eb036db1da7077b
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2019-10-11 16:18:17 -0400

    Add view to return gcr images.

--- django/app/views.py
+++ django/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View as RegView
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+from .images import list_images
+
+
+class ListImagesView(APIView):
+    def get(self, request, format=None):
+        return Response(list_images())
