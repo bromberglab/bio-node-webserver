b462d521e2e6cae8dd46c61431a02274e783f719
Yannick Spreen <yannick.spreen@gmail.com>
2019-10-15 11:54:11 -0400

    Add storage web hook.

--- create-deployment.sh
+++ create-deployment.sh
@@ -2,3 +2,7 @@
 
 gcloud compute addresses create server-address --global
 kubectl apply -f deployment.yml
+
+[ -f gs_secret.txt ] || LC_ALL=C tr -dc "A-Za-z0-9-_" </dev/urandom | head -c 20 > gs_secret.txt
+echo "gs_secret: $(cat gs_secret.txt | tr -d '\n' | base64)"
+gsutil notification watchbucket -t "$(cat gs_secret.txt)" https://bio-no.de/webhooks/gs_update/ gs://artifacts.poised-cortex-254814.appspot.com
