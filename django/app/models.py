035ba0c9d2b6718344534890e48532aeeb397b7a
Yannick Spreen <yannick.spreen@gmail.com>
2019-10-16 15:43:41 -0400

    Add missing migration.

--- django/app/models.py
+++ django/app/models.py
@@ -9,8 +9,8 @@ class CronJob(models.Model):
 
 class NodeImage(models.Model):
     name = models.CharField(max_length=128, primary_key=True)
-    labels_string = models.TextField()
-    cmd_string = models.TextField()
+    labels_string = models.TextField(default='')
+    cmd_string = models.TextField(default='')
 
     @property
     def labels(self):
