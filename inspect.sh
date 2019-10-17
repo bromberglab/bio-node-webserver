e139ca0f8c0df71ab376b58ce21ee63d7e1bba19
Yannick Spreen <yannick.spreen@gmail.com>
2019-10-17 16:29:19 -0400

    Optimize inspect.sh.

--- inspect.sh
+++ inspect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 docker pull "$1"
-docker image inspect "$1" | jq '.[0]["Config"]["Labels"] // {}' > image_labels.json
-docker image inspect "$1" | jq '.[0]["Config"]["Entrypoint"] // []' > image_entrypoint.json
-docker image inspect "$1" | jq '.[0]["Config"]["Cmd"] // []' > image_cmd.json
-docker image inspect "$1" | jq '.[0]["Config"]["Env"] // []' > image_env.json
+docker image inspect "$1" > image_meta.json
+cat image_meta.json | jq '.[0]["Config"]["Labels"] // {}' > image_labels.json
+cat image_meta.json | jq '.[0]["Config"]["Entrypoint"] // []' > image_entrypoint.json
+cat image_meta.json | jq '.[0]["Config"]["Cmd"] // []' > image_cmd.json
+cat image_meta.json | jq '.[0]["Config"]["Env"] // []' > image_env.json
 docker image rm "$1"
