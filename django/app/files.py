28606b673f9e96709c7ff0367ec203bd0a4fcd75
Yannick Spreen <yannick.spreen@gmail.com>
2019-10-25 22:01:54 -0400

    More solid file upload.

--- django/app/files.py
+++ django/app/files.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import random
 import string
+from app.models import *
 
 base_path = Path(settings.DATA_PATH)
 base_path /= 'data'
@@ -12,18 +13,18 @@ base_path /= 'data'
 rnd = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
 
 
-def _handle_uploaded_file(data):
-    threading.Thread(target=lambda: _handle_uploaded_file(data)).start()
+def _handle_uploaded_file(request):
+    threading.Thread(target=lambda: _handle_uploaded_file(request)).start()
 
 
-def handle_uploaded_file(data):
-    # TODO: Enforce closing of uploads, so that existing uploads cannot be overridden by anyone.
+def handle_uploaded_file(request):
+    data = request.data
+    upload = get_upload(request)
 
     file = data['file']
     chunkNumber = data['chunkNumber']
     totalChunks = int(data['totalChunks'])
-    uuid = data['identifier']
-    uuid = uuid.split("/")[0]
+    uuid = str(upload.uuid)
     relativePath = data['relativePath']
     filename = data['filename']
     file_type = data.get('type', 'file')
@@ -37,6 +38,9 @@ def handle_uploaded_file(data):
 
 
 def save_file(file, path: Path, totalChunks=0, filename="file"):
+    suffix = '.partial_chunk'
+
+    path = path.with_suffix(suffix)
     os.makedirs(path.parent, exist_ok=True)
     with open(path, 'wb+') as destination:
         for chunk in file.chunks():
@@ -46,7 +50,26 @@ def save_file(file, path: Path, totalChunks=0, filename="file"):
     if num_files == totalChunks:
         with open(path.parent.parent / filename, 'wb+') as wfd:
             for i in range(totalChunks):
-                with open(path.parent / str(i+1), 'rb+') as fd:
+                partial_path = path.parent / str(i+1)
+                partial_path = partial_path.with_suffix(suffix)
+
+                with open(partial_path, 'rb+') as fd:
                     shutil.copyfileobj(fd, wfd)
-                os.remove(path.parent / str(i + 1))
+                os.remove(partial_path)
             os.rmdir(path.parent)
+
+
+def get_upload(request):
+    pk = request.session.get('upload_pk', None)
+    upload = None
+    if pk is not None:
+        upload = Upload.objects.get(pk=pk)
+    if upload is None or upload.is_finished:
+        upload = Upload()
+        if request.user.is_authenticated:
+            upload.user = request.user
+        upload.save()
+        pk = upload.pk
+        request.session['upload_pk'] = str(pk)
+
+    return upload
