742f0825bc72660324f6a77c7f67d2fe6fd9f7f5
Yannick Spreen <yannick.spreen@gmail.com>
2019-11-14 15:29:26 -0500

    Set download filename.

--- django/config/urls.py
+++ django/config/urls.py
@@ -34,7 +34,9 @@ api = [
     path("workflow_storage/", WorkflowStorageView.as_view()),
     path("workflow_run/", WorkflowRunView.as_view()),
     path("create_download/", CreateDownload.as_view()),
-    path("download/<str:name>/", DownloadView.as_view(), name="download"),
+    path(
+        "download/<str:name>/<str:filename>/", DownloadView.as_view(), name="download"
+    ),
     path("names_for_type/", NamesForTypeView.as_view()),
     path("show_cookie_info/", CookieInfoView.as_view()),
     path("notification/", NotificationView.as_view()),
