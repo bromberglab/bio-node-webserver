575447efe131d7889cb7affa7744b794f97b822d
Yannick Spreen <yannick.spreen@gmail.com>
2019-11-13 22:26:36 -0500

    Add zip streams.

--- django/app/management/commands/downloads.py
+++ django/app/management/commands/downloads.py
@@ -2,11 +2,12 @@ import shutil
 from django.utils import timezone
 from pathlib import Path
 from app.models import *
+import os
 
 
 def cron():
     limit = timezone.now()
     limit -= timezone.timedelta(hours=2)
     for d in Download.objects.filter(created_at__lte=limit):
-        shutil.rmtree(Path(d.path).parent)
+        os.remove(Path(d.path))
         d.delete()
--- django/config/urls.py
+++ django/config/urls.py
@@ -34,6 +34,7 @@ api = [
     path("workflow_storage/", WorkflowStorageView.as_view()),
     path("workflow_run/", WorkflowRunView.as_view()),
     path("create_download/", CreateDownload.as_view()),
+    path("download/<str:name>/", DownloadView.as_view()),
     path("names_for_type/", NamesForTypeView.as_view()),
     path("show_cookie_info/", CookieInfoView.as_view()),
     path("notification/", NotificationView.as_view()),
