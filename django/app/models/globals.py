eca15bf81f68e598f2070657974998929489a3de
Yannick Spreen <yannick.spreen@gmail.com>
2019-11-18 14:39:57 -0500

    Save random string in db.

--- django/app/models/globals.py
+++ django/app/models/globals.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid as uu
 
 
 class Globals(models.Model):
+    some_uuid = models.UUIDField(default=uu.uuid4)
     gs_webhook_working = models.BooleanField(default=False)
     gs_webhook_fired = models.BooleanField(default=False)
     log_chars_kept = models.IntegerField(default=1000)
@@ -17,3 +19,9 @@ class Globals(models.Model):
 
         return i
 
+    @property
+    def random(self):
+        r = str(self.some_uuid)
+        r = r.split("-")[0]
+
+        return r
