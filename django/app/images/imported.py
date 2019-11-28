3f11a2ff642789ceda3a99f8fe97fdae1408626c
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2019-11-28 00:41:45 -0500

    Fix reset.

--- django/app/images/imported.py
+++ django/app/images/imported.py
@@ -52,21 +52,21 @@ def change_image(name, user=None, data=None):
         return
 
     image = NodeImage.objects.get(name=name)
-
     image.override_string = "{}"
 
-    for k in image.labels:
-        try:
-            data["labels"][k]
-        except:
-            data["labels"][k] = None
-    for k in image.env:
-        try:
-            data["env"][k]
-        except:
-            data["env"][k] = None
+    if len(data):
+        for k in image.labels:
+            try:
+                data["labels"][k]
+            except:
+                data["labels"][k] = None
+        for k in image.env:
+            try:
+                data["env"][k]
+            except:
+                data["env"][k] = None
+        image.override = data
 
-    image.override = data
     image.save()
 
     return image
