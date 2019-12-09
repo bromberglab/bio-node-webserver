a9b88df53b42e8809242946530398f427f31c31c
Yannick Spreen <yannick.spreen@gmail.com>
2019-12-09 16:23:51 -0500

    Change base image.

--- util.sh
+++ util.sh
@@ -1,7 +1,7 @@
 openssl aes-256-cbc -K "$encryption_key" -iv "$encryption_key"2 -in webservice-key.json -out webservice-key.json.enc
 
 docker build . -f sdk.Dockerfile -t gsdk
-docker tag gsdk gcr.io/poised-cortex-254814/cloudsdk
-docker push gcr.io/poised-cortex-254814/cloudsdk
+docker tag gsdk yspreen/gcloudsdk-plus
+docker push yspreen/gcloudsdk-plus
 
 docker system prune -a
