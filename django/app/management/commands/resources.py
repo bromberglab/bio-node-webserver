fa7bd500be5c2cc91abf6e446310b6781048dd99
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2019-12-10 18:00:33 -0500

    Regex -> re.

--- django/app/management/commands/resources.py
+++ django/app/management/commands/resources.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from app.models import *
 from app.kube import get_resources
-import regex
+import re
 
 reg = r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}(-\d+)?-.{5}$"
 
@@ -9,7 +9,7 @@ reg = r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}(-\d+)?-.{5
 def cron():
     res = get_resources()
     for k, v in res.items():
-        if regex.match(reg, k):
+        if re.match(reg, k):
             ResourceUsage.update(k, *v)
 
 
