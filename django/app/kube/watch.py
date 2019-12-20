92cc1ec1ce540dd6dc0f0e18905b7de9501bc439
Yannick Spreen <yannick.spreen@gmail.com>
2019-12-20 15:10:34 -0500

    Add debug prints.

--- django/app/kube/watch.py
+++ django/app/kube/watch.py
@@ -6,6 +6,9 @@ import threading
 from .jobs import create_logfile
 
 
+DEBUG_WATCH = False
+
+
 def retry(fun, times=4, wait=4, fail=False):
     error = None
     while times > 0:
@@ -25,12 +28,16 @@ def handle_status(api, k8s_batch_v1, job_name, pod, status):
     from app.management.commands.resources import reg
 
     if not re.match(reg, pod):
+        if DEBUG_WATCH:
+            print("no match")
         return
 
     try:
         # job name is 'uuid-num', so we take the first 36 chars
         job = Job.objects.get(uuid=job_name[:36])
     except:
+        if DEBUG_WATCH:
+            print("no job")
         return
 
     logs = retry(lambda: api.read_namespaced_pod_log(name=pod, namespace="default"))
@@ -41,6 +48,8 @@ def handle_status(api, k8s_batch_v1, job_name, pod, status):
         fail=True,
     )
     retry(lambda: api.delete_namespaced_pod(str(pod), namespace="default"), fail=False)
+    if DEBUG_WATCH:
+        print("handling")
 
     job.handle_status(status, pod=pod)
     create_logfile(pod, logs)
@@ -52,24 +61,29 @@ def status_thread(api, k8s_batch_v1, lock, pods, tasks):
     while True:
         time.sleep(0.1)
         el = None
-        try:
-            with lock:
-                el = list[0]
-                del list[0]
-        except:
-            pass
+        with lock:
+            if len(tasks) > 0:
+                el = tasks[0]
+                del tasks[0]
 
         if el is None:
             continue
 
+        if DEBUG_WATCH:
+            print("new task", el)
+
         try:
             r = handle_status(api, k8s_batch_v1, *el)
+            if DEBUG_WATCH:
+                print("handle_status", r)
             if r == 1:
+                job = el[0]
                 with lock:
-                    job = el[0]
-                    del pods[job]
+                    if pods.get(job, None) is not None:
+                        del pods[job]
         except Exception as e:
-            print("Handling error:", e)
+            if DEBUG_WATCH:
+                print("Handling error:", e)
             # traceback.print_exc()
 
 
@@ -113,10 +127,13 @@ def get_status_all():
             status = "succeeded" if success else ("failed" if failure else None)
 
             if status is not None:
-                try:
-                    with lock:
-                        pod = pods[job]
-                except:
+                if DEBUG_WATCH:
+                    print("new job", job, status)
+                with lock:
+                    pod = pods.get(job, None)
+                if pod is None:
+                    if DEBUG_WATCH:
+                        print("no pod")
                     continue
                 with lock:
                     tasks.append((job, pod, status))
