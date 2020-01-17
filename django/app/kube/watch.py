324041d3ff37c35f68924849adf0ad9e803eeea7
Yannick Spreen <yannick.spreen@gmail.com>
2020-01-17 12:59:21 -0500

    Add logging.

--- django/app/kube/watch.py
+++ django/app/kube/watch.py
@@ -6,9 +6,40 @@ import threading
 import urllib3.exceptions
 from .jobs import create_logfile
 from django.utils.timezone import now
+from django.conf import settings
+import json
+from app.util import now, dtformat
 
 
-DEBUG_WATCH = False
+DEBUG_WATCH = True
+
+
+def debug_print(*msg, high_frequency=False):
+    if not DEBUG_WATCH:
+        return
+
+    msg = str(msg)
+    fullmsg = "[%s] %s" % (now().strftime(dtformat), msg)
+
+    logdir = settings.BASE_DIR + "/watch.log"
+    hflogdir = settings.BASE_DIR + "/watch.log.latest"
+
+    if settings.DEBUG:
+        print(fullmsg)
+
+    if not high_frequency:
+        with open(logdir, "a") as f:
+            f.write(str(fullmsg) + "\n")
+    else:
+        try:
+            with open(hflogdir, "r") as f:
+                logs = f.read()
+            logs = json.loads(logs)
+        except:
+            logs = {}
+        logs[msg] = fullmsg
+        with open(hflogdir, "w") as f:
+            json.dump(logs, f)
 
 
 def retry(fun, times=2, wait=0.1, fail=False):
@@ -37,8 +68,7 @@ def handle_status(
     from app.management.commands.resources import reg
 
     if not re.match(reg, pod):
-        if DEBUG_WATCH:
-            print("no match")
+        debug_print("no match")
         return
 
     if pod in handled_pods:
@@ -49,8 +79,7 @@ def handle_status(
         # remove bio prefix
         job = Job.objects.get(uuid=job_name[3:39])
     except:
-        if DEBUG_WATCH:
-            print("no job")
+        debug_print("no job")
         retry(
             lambda: k8s_batch_v1.delete_namespaced_job(job_name, namespace="default"),
             fail=False,
@@ -77,14 +106,12 @@ def handle_status(
     )
 
     if status != "succeeded" and job.retries_left > 0:
-        if DEBUG_WATCH:
-            print("retry")
+        debug_print("retry")
 
         logs += "\nRetrying run ... (%d left)" % job.retries_left
         job.retry(job_name)
     else:
-        if DEBUG_WATCH:
-            print("handling")
+        debug_print("handling")
 
         job.handle_status(status, pod=pod)
     create_logfile(pod, logs)
@@ -100,6 +127,7 @@ def status_thread(api, k8s_batch_v1, lock, pods, tasks, unhandled_pods, unhandle
     unhandled_check = now()
 
     while True:
+        debug_print("loop status_thread", high_frequency=True)
         time.sleep(0.1)
         el = None
         with lock:
@@ -110,21 +138,18 @@ def status_thread(api, k8s_batch_v1, lock, pods, tasks, unhandled_pods, unhandle
         if el is None:
             continue
 
-        if DEBUG_WATCH:
-            print("new task", el)
+        debug_print("new task", el)
 
         try:
             r = handle_status(api, k8s_batch_v1, unhandled_pods, unhandled_jobs, *el)
-            if DEBUG_WATCH:
-                print("handle_status", r)
+            debug_print("handle_status", r)
             if r == 1:
                 job = el[0]
                 with lock:
                     if pods.get(job, None) is not None:
                         del pods[job]
         except Exception as e:
-            if DEBUG_WATCH:
-                print("Handling error:", e)
+            debug_print("Handling error:", e)
             # traceback.print_exc()
 
         if (now() - unhandled_check).total_seconds() > 5 * 60:  # 5min
@@ -179,6 +204,7 @@ def pod_thread(lock, pods, api, unhandled_pods, unhandled_jobs):
     w = watch.Watch()
 
     while True:
+        debug_print("loop pod_thread", high_frequency=True)
         for event in w.stream(
             api.list_namespaced_pod, namespace="default", timeout_seconds=120
         ):
@@ -218,6 +244,7 @@ def get_status_all():
     time.sleep(3)
 
     while True:
+        debug_print("loop get_status_all", high_frequency=True)
         try:
             for event in w.stream(
                 k8s_batch_v1.list_namespaced_job,
@@ -232,13 +259,11 @@ def get_status_all():
                 status = "succeeded" if success else ("failed" if failure else None)
 
                 if status is not None:
-                    if DEBUG_WATCH:
-                        print("new job", job, status)
+                    debug_print("new job", job, status)
                     with lock:
                         pod = pods.get(job, None)
                     if pod is None:
-                        if DEBUG_WATCH:
-                            print("no pod")
+                        debug_print("no pod")
                         if unhandled_jobs.get(job, None) is None:
                             unhandled_jobs[job] = now()
                         continue
@@ -251,8 +276,7 @@ def get_status_all():
                                 t = list(t)
                                 t[1] = pod
                                 tasks[i] = tuple(t)
-                                if DEBUG_WATCH:
-                                    print(job, "already enlisted")
+                                debug_print(job, "already enlisted")
                                 exists = True
                                 break
                         if not exists:
