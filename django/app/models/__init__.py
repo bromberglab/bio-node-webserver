3dd0b554e58e586393bd351122bc44ead5ce71e6
Yannick Spreen <yannick.spreen@gmail.com>
2020-01-31 16:02:54 -0500

    Add API workflows.

--- django/app/models/__init__.py
+++ django/app/models/__init__.py
@@ -4,7 +4,7 @@ from .globals import Globals
 from .job import Job
 from .node_image import FileType, NodeImage, NodeImageTag
 from .upload import Upload
-from .workflow import Workflow
+from .workflow import Workflow, ApiWorkflow
 from .notification import Notification
 from .permissions import Permissions
 from .resource_usage import ResourceUsage
