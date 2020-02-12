b846aa41ca495a83e4cc3d288c192ae56e4e8c8b
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-02-12 16:29:03 -0500

    Add auto logout for guests.

--- django/app/middleware.py
+++ django/app/middleware.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
+from django.utils import timezone
+from django.contrib.auth import logout
 
 
 class HttpRedirectMiddleware:
@@ -14,6 +16,23 @@ class HttpRedirectMiddleware:
                 path += request.META.get("HTTP_HOST")
                 path += request.get_full_path()
                 return redirect(path)
+
+        response = self.get_response(request)
+
+        return response
+
+
+class AutoLogout:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        logout_at = request.session.get("logout_at", None)
+        if logout_at is not None:
+            if (
+                timezone.now() - timezone.datetime.fromisoformat(logout_at)
+            ).total_seconds() > 0:
+                logout(request)
 
         response = self.get_response(request)
 
--- django/config/settings.py
+++ django/config/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "app.middleware.DisableCSRF",
+    "app.middleware.AutoLogout",
 ]
 
 CORS_ORIGIN_WHITELIST = []
