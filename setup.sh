29609e2307c37cd29eeaa26136fcb4d8446e3d27
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-02-26 15:43:28 -0500

    Change reboot to 3 AM every second day.

--- setup.sh
+++ setup.sh
@@ -9,3 +9,13 @@ mkdir /volume/logs/django
 mkdir /volume/logs/daemon
 rm /volume/logs/daemon/watch.log
 export GOOGLE_APPLICATION_CREDENTIALS="/keys/sa-key.json"
+
+cron_reboot() {
+    HOUR=3 # TZ=EST
+    secondsleft=$(python3 -c 'import datetime; print(24*60*60 - int((datetime.datetime.now() - datetime.datetime.now().replace(hour='$HOUR', minute=0, second=0)).total_seconds()))')
+    sleep ${secondsleft}s
+    sleep 1d
+    reboot
+}
+
+cron_reboot &
