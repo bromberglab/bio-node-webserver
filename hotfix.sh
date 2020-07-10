31eec941334880c21b85656478c758dc0a9e2d13
Yannick Spreen <yannick.spreen@gmail.com>
2020-07-10 10:20:45 +0200

    Remove old scripts; Change readme.

--- hotfix.sh
+++
@@ -1,4 +0,0 @@
-docker pull gcr.io/poised-cortex-254814/webservice-server:latest
-docker build -f hotfix.dockerfile -t gcr.io/poised-cortex-254814/webservice-server .
-docker push gcr.io/poised-cortex-254814/webservice-server:latest
-kubectl delete pod -l "app=server"
