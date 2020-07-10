3a0e5d5886503d436fd95e89ba82e9e00cad8144
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-07-10 10:20:45 +0200

    Remove old scripts; Change readme.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# webservice-server
+# Bio-Node
+
+## For Documentation, see [this](https://github.com/bromberglab/bio-node-docs) repo.
+
+### Further Links
+
+Website: https://bio-no.de  
+Runtime Container: https://github.com/bromberglab/bio-node  
+Docs: https://github.com/bromberglab/bio-node-docs  
+Web Client: https://github.com/bromberglab/bio-node-webclient  
+Web Server: https://github.com/bromberglab/bio-node-webserver  
+Filter Containers: https://github.com/bromberglab/bio-node-filters
--- release.sh
+++
@@ -1,9 +0,0 @@
-docker pull gcr.io/poised-cortex-254814/webservice-server:latest
-docker tag gcr.io/poised-cortex-254814/webservice-server bromberglab/bio-node-webserver
-docker push bromberglab/bio-node-webserver
-docker pull gcr.io/poised-cortex-254814/webservice-client:latest
-docker tag gcr.io/poised-cortex-254814/webservice-client bromberglab/bio-node-webclient
-docker push bromberglab/bio-node-webclient
-docker pull gcr.io/poised-cortex-254814/webservice-docs:latest
-docker tag gcr.io/poised-cortex-254814/webservice-docs bromberglab/bio-node-docs
-docker push bromberglab/bio-node-docs
