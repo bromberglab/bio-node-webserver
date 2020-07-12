9aaacab98fde98998520c407379d69820a0a68c5
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2020-07-12 17:34:21 +0200

    Update python requirements.

--- util.sh
+++ util.sh
@@ -1,1 +1,4 @@
 docker system prune -a
+
+# update pip:
+deactivate; rm -rf .pyenv; python3 -m virtualenv .pyenv; source .pyenv/bin/activate; pip install --upgrade pip; pip install -r requirements.update.txt; pip freeze > requirements.txt
